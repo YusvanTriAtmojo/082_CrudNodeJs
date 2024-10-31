c98aa8b500434ea5b552ce0004ebb3385662d20a
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-10-31 15:33:39 +0700

    CRUD_RESTAPI

--- routes/Hewan.js
+++ routes/Hewan.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const hewan = [
     {
+        id : 1,
         nama: "Mbapee",
         jenis: "Kura-kura",
         habitat: "Darat & Air",
@@ -12,6 +13,7 @@ const hewan = [
         reproduksi: "Bertelur",
     },
     {
+        id : 2,
         nama: "Willy",
         jenis: "Paus Orca",
         habitat: "Air",
@@ -20,6 +22,7 @@ const hewan = [
         reproduksi: "Beranak",
     },
     {
+        id : 3,
         nama: "Moly",
         jenis: "Kucing",
         habitat: "Darat",
@@ -31,5 +34,44 @@ const hewan = [
 
 router.get("/", (req,res) => {
     res.send(hewan);
+});
+
+router.post("/", (req, res) => {
+    const newHewan = {
+        id: hewan.length + 1,
+        nama: req.body.nama,
+        jenis: req.body.jenis,
+        habitat: req.body.habitat,
+        umur: req.body.umur,
+        makanan: req.body.makanan,
+        reproduksi: req.body.reproduksi,
+    };
+
+    hewan.push(newHewan);
+    res.status(201).json(newHewan);
+});
+
+router.delete('/:id', (req, res) => {
+    const hewanIndex = hewan.findIndex(t => t.id === parseInt(req.params.id));
+    if (hewanIndex === -1) return res.status(404).json({ message: 'Hewan tidak ditemukan' });
+
+    const deletedHewan = hewan.splice(hewanIndex, 1)[0]; //menghapus dan menyimpan hewan yang dihapus
+    res.status(200).json({ message: `Hewan '${deletedHewan.nama}' telah dihapus`});
+});
+
+router.put('/:id', (req, res) => {
+    const animal = hewan.find(t => t.id === parseInt(req.params.id));
+    if (!animal) return res.status(404).json({ message: 'Hewan tidak ditemukan' });
+    animal.nama = req.body.nama || animal.nama;
+    animal.jenis = req.body.jenis || animal.jenis;
+    animal.habitat = req.body.habitat || animal.habitat;
+    animal.umur = req.body.umur || animal.umur;
+    animal.makanan = req.body.makanan || animal.makanan;
+    animal.reproduksi = req.body.reproduksi || animal.reproduksi;
+
+    res.status(200).json({
+        message: `Hewan dengan ID ${animal.id} telah diperbarui`,
+        updatedHewan: animal
+    });
 });
 export default router;
--- routes/Mobil.js
+++ routes/Mobil.js
@@ -4,12 +4,14 @@ const router = express.Router();
 
 const mobil = [
     {
+        id: 1,
         merk: "Toyota",
         model: "Avanza",
         warna: "Hitam",
         tahun: 2021,
     },
     {
+        id: 2,
         merk: "Porshe",
         model: "Sport",
         warna: "Kuning",
@@ -19,4 +21,40 @@ const mobil = [
 router.get("/", (req,res) => {
     res.send(mobil);
 });
+
+router.post("/", (req, res) => {
+    const newMobil = {
+        id: mobil.length + 1,
+        merk: req.body.merk,
+        model: req.body.model,
+        warna: req.body.warna,
+        tahun: req.body.tahun,
+    };
+
+    mobil.push(newMobil);
+    res.status(201).json(newMobil);
+});
+
+router.delete('/:id', (req, res) => {
+    const mobilIndex = mobil.findIndex(t => t.id === parseInt(req.params.id));
+    if (mobilIndex === -1) return res.status(404).json({ message: 'Mobil tidak ditemukan' });
+
+    const deletedMobil = mobil.splice(mobilIndex, 1)[0]; //menghapus dan menyimpan hewan yang dihapus
+    res.status(200).json({ message: `Mobil '${deletedMobil.merk}' telah dihapus`});
+});
+
+router.put('/:id', (req, res) => {
+    const car = mobil.find(t => t.id === parseInt(req.params.id));
+    if (!car) return res.status(404).json({ message: 'Mobil tidak ditemukan' });
+    car.merk = req.body.merk || car.merk;
+    car.model = req.body.model || car.model;
+    car.warna = req.body.warna || car.warna;
+    car.tahun = req.body.tahun || car.tahun;
+
+    res.status(200).json({
+        message: `Mobil dengan ID ${car.id} telah diperbarui`,
+        updatedMobil: car
+    });
+});
+
 export default router;
